c98eb54ebdba1472f293cf95c005c9ab6a48d649
mehradsadeghi <2012.linkinpark@gmail.com>
2020-01-17 11:12:55 +0330

    InsertionSort and its tests are done.

---
+++ src/Sort/InsertionSort.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Src\Sort;
+
+class InsertionSort {
+
+    /**
+     * Sorts array of un-sorted items in ascending order
+     * Based on insertion sort algorithm
+     *
+     * @param array $items
+     * @return array
+     */
+    public function sort(array $items): array {
+
+        $count = count($items);
+
+        for($i = 1; $i < $count; $i++) {
+
+            $j = $i;
+
+            while($j > 0 and $items[$j - 1] > $items[$j]) {
+
+                $key = $items[$j];
+                $items[$j] = $items[$j - 1];
+                $items[$j - 1] = $key;
+                $j--;
+            }
+        }
+
+        return $items;
+    }
+}
---
+++ tests/Sort/InsertionSortTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Src\Sort\InsertionSort;
+
+class InsertionSortTest extends TestCase {
+
+    private $insertionSort;
+    private const UNSORTED_LIST = [2, 5, 7, 6, 4, 3, 1];
+    private const SORTED_LIST = [1, 2, 3, 4, 5, 6, 7];
+
+    public function setUp(): void {
+        parent::setUp();
+        $this->insertionSort = new InsertionSort();
+    }
+
+    public function testIfCanSortCorrectly() {
+        $this->assertEquals(self::SORTED_LIST, $this->insertionSort->sort(self::UNSORTED_LIST));
+    }
+}
